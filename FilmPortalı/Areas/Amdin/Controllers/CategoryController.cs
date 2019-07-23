19ad502a6b71f606d4506d388ea92a912368fe97
Oktay Donmez <oktay.donmez98@hotmail.com>
2019-07-23 13:12:10 +0300

    kategori ekleme

---
+++ FilmPortalı/Areas/Amdin/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using FilmPortalı.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmPortalı.Areas.Amdin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        FilmPortaliEntities db = new FilmPortaliEntities();
+        // GET: Amdin/Category
+        public ActionResult Index()
+        {
+            List<Categories> cat = db.Categories.ToList(); 
+            return View(cat);
+        }
+
+        public ActionResult Add()
+        {
+            return View("CategoryForm");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var model = db.Categories.Find(id);
+            if (model == null) return HttpNotFound();
+            return View("CategoryForm", model);
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            var model = db.Categories.Find(id);
+            if (model == null) return HttpNotFound();
+            db.Categories.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Save(Categories cat)
+        {
+            if (cat.CId == 0) db.Categories.Add(cat);
+            else db.Entry(cat).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
